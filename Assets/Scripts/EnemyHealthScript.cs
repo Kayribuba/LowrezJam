dc43338b56efd9d80143bd9b930fa70ee4dec7b0
Kayribuba <70769641+Kayribuba@users.noreply.github.com>
2022-08-02 21:42:53 +0300

    asd

--- Assets/Scripts/EnemyHealthScript.cs
+++ Assets/Scripts/EnemyHealthScript.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 
 public class EnemyHealthScript : MonoBehaviour
 {
+    [SerializeField] AnimationClip[] animations;
+    [SerializeField] Animation anim;
+
     [SerializeField] float maxHealth = 6f;
     [SerializeField] bool destroyAfterDeath = true;
 
     float health;
+    int animPhase;
     bool dead;
 
     void Start()
     {
         health = maxHealth;
+
+        for(int i = 0; i < animations.Length; i++)
+        {
+            anim.AddClip(animations[i], "Phase" + i);
+        }
     }
 
     public void TakeDamage()
     {
         if (!dead)
         {
+            anim.Play("Phase"+animPhase);
+            if(animPhase < animations.Length)
+            animPhase++;
+
             health--;
             Debug.Log($"Enemy {gameObject.name} lot health. Remaining health : {maxHealth}/{health}.");
 
@@ -34,7 +47,7 @@ public class EnemyHealthScript : MonoBehaviour
     {
         dead = true;
         Debug.Log($"Enemy {gameObject.name} has died.");
-
+        
         if (destroyAfterDeath)
         {
             DestroyEnemy();
