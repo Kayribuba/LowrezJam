c192819492a3bb0418554b6435977dc11ad1b17f
Kayribuba <70769641+Kayribuba@users.noreply.github.com>
2022-08-13 17:50:29 +0300

    a

--- Assets/Scripts/ParallaxCode.cs
+++ Assets/Scripts/ParallaxCode.cs
@@ -2,15 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class ParallaxCode : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject[] spritesToMove;
+
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
+        GameObject GO = Instantiate(spritesToMove[0], transform.position, Quaternion.identity);
+        float firstGridY = GO.transform.position.y - gameObject.transform.lossyScale.y / 2;
+        GO.transform.position = new Vector3(transform.position.x, firstGridY, -10);
+
+        //int i = 1;
+        //foreach(GameObject sprite in spritesToMove)
+        //{
+        //    float targetSize = transform.localScale.x / sprite.transform.localScale.x;
+        //    sprite.transform.localScale *= targetSize;
+
+        //    GameObject GOInstantiated = Instantiate(sprite, transform.position, Quaternion.identity);
+
+        //    Debug.Log(GOInstantiated.transform.lossyScale + " " + GOInstantiated.transform.localScale);
+
+        //Vector2 targetLocation = transform.position;
+        //targetLocation.y -= transform.lossyScale.y / 2;
+        //targetLocation.y += sprite.transform.lossyScale.y / 2;
+        //GOInstantiated.transform.position = targetLocation;
+
+        //    GOInstantiated.GetComponent<SpriteRenderer>().sortingLayerName = GetComponent<SpriteRenderer>().sortingLayerName;
+        //    GOInstantiated.GetComponent<SpriteRenderer>().sortingOrder = GetComponent<SpriteRenderer>().sortingOrder + i;
+        //    i++;
+    //   }
+}
     void Update()
     {
         
