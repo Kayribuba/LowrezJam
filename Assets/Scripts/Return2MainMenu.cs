83038f618cf70f2c48d39afad6038ca3aba0bacb
bananaizg0d <someonefarted99@gmail.com>
2022-08-15 02:11:25 +0300

    aaa

---
+++ Assets/Scripts/Return2MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Return2MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Return2Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void ANAN()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
